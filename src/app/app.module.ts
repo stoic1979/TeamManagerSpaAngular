f4e900c510d2a78b57bb61965becc55d17608760
Barinderkumar <barinderkumar99@gmail.com>
2018-01-13 17:06:36 +0530

    updted code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DashbaordComponent
   ],
   imports: [
     BrowserModule,
